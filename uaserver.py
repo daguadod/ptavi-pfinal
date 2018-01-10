6701cf1caa8f0e7dcd6a1c76ce960ae757a1de44
David Aguado Dianez <daguadod@alpha50.aulas.gsyc.urjc.es>
2018-01-10 17:43:50 +0100

    Depurando uaserver  y resueltos problemas de codigo

--- uaserver.py
+++ uaserver.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import time
+import socket
 import socketserver
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
@@ -38,12 +39,12 @@ parser.setContentHandler(cHandler)
 parser.parse(open(FICHERO))
 tags = cHandler.get_tags()
 
-def log(log_file, evento):
+def log(log_file, event):
 
-	fichero = open(log_file, 'a')
+	fichero = open(log_file,"a")
 	tiempo = time.gmtime(time.time())
-	fichero.write(time.strftime('%Y%m%d%H%M%S', tiempo))
-	fichero.write(event.replace('\r\n', ' ') + '\r\n')
+	fichero.write(time.strftime("%Y%m%d%H%M%S", tiempo))
+	fichero.write(event.replace("\r\n", " ") + "\r\n")
 	fichero.close()
 
 METHOD = {
@@ -59,7 +60,6 @@ if My_IP == "":
 	My_IP = "127.0.0.1"
 My_Name = tags["account_username"]
 My_Password = tags["account_passwd"]
-My_IP = tags["uaserver_ip"]
 My_Port = tags["uaserver_puerto"]
 Proxy_IP = tags["regproxy_ip"]
 Proxy_Port = int(tags["regproxy_puerto"])
@@ -79,7 +79,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 		linea = self.rfile.read().decode('utf-8')
 		LINE = linea.split()
-		
+		print("Received :" + linea)
 		address = self.client_address[0] + ":"+ str(self.client_address[1])
 		event = " Received from" + Proxy_IP + ":" + str(Proxy_Port) + ": " + linea
 		log(log_file, event)
@@ -117,12 +117,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 			log(log_file, event)
 
 		elif LINE[0] == "BYE":
+		
 			ANSW = METHOD["BYE"]
 			self.wfile.write(bytes(ANSW, 'utf-8'))
-			print("Recibido" + str(line))
+			print("Recibido" + str(ANSW))
 
-			event = ' Sent to ' + proxy_IP + ':'
-			event += proxy_port + ': ' + ANSW
+			event = ' Sent to ' + Proxy_IP + ':'
+			event += str(Proxy_Port) + ': ' + ANSW
 			log(log_file, event)
 
 		elif not LINE[0] in METHOD:
