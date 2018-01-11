7df4032d72a8d5b3a8d373d967a11fc90bf074de
David Aguado Dianez <daguadod@alpha48.aulas.gsyc.urjc.es>
2018-01-11 19:15:10 +0100

    pasado PEP8 y solucionados problemas de idioma en uaclient

--- uaclient.py
+++ uaclient.py
@@ -6,6 +6,7 @@ import sys
 import time
 import socket
 import hashlib
+import threading
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
@@ -84,7 +85,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
                 + "Expires: " + OPTION + "\r\n\r\n"
         my_socket.send(bytes(LINE, 'utf-8'))
         print("Sending: " + LINE)
-        event = " sent to " + Proxy_IP + ":" + str(Proxy_Port) + ":" + LINE
+        event = " Sent to " + Proxy_IP + ":" + str(Proxy_Port) + ":" + LINE
         log(log_file, event)
         try:
             data = my_socket.recv(1024)
@@ -98,7 +99,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
                 m.update(bytes(nonce, 'utf-8'))
                 m.update(bytes(My_Password, 'utf-8'))
                 response = m.hexdigest()
-                LINE += 'Authorization: Digest response=' + response
+                LINE += "Authorization: Digest response=" + response
                 print('Sending ' + LINE)
 
                 my_socket.send(bytes(LINE, 'utf-8') + b"\r\n\r\n")
@@ -106,11 +107,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
                 event += Proxy_Port + ': ' + LINE
                 log(log_file, event)
 
-                data_recv = my_socket.recv(int(Proxy_Port))
+                data_recv = my_socket.recv(1024)
 
-                evento = ' Received from ' + Proxy_IP + ':'
-                evento += Proxy_Port + ': ' + data.decode('utf-8')
-                log(log_file, evento)
+                event = " Received from " + Proxy_IP + ":"
+                event += Proxy_Port + ': ' + data.decode('utf-8')
+                log(log_file, event)
 
         except ConnectionRefusedError:
             sys.exit("Connection Refused")
@@ -118,12 +119,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     elif METHOD == "INVITE":
         SDPHEAD = "\r\n\r\nContent_Type: application/sdp\r\n" + "v=0\r\n"
         SDPHEAD += "o=" + My_Name + " " + My_IP  \
-            + "\r\ns=" + "sesionPrueba\r\nt=0\r\nm=audio " \
+            + "\r\ns=" + "mysesion\r\nt=0\r\nm=audio " \
             + tags["rtpaudio_puerto"] + " RTP"
         LINE = METHOD + " sip:" + OPTION \
             + " SIP/2.0" + SDPHEAD
         my_socket.send(bytes(LINE, 'utf-8'))
-        print("Enviando: " + LINE)
+        print("Sending: " + LINE)
         event = ' Sent to ' + Proxy_IP + ':'
         event += str(Proxy_Port) + ': ' + LINE
         log(log_file, event)
@@ -133,7 +134,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
             sys.exit("ConnectionRefused")
         decode = data.decode('utf-8')
         DECODED = data.decode('utf-8').split()
-        event = ' Received from ' + Proxy_IP + ':'
+        event = " Received from " + Proxy_IP + ":"
         event += str(Proxy_Port) + ': ' + data.decode('utf-8')
         log(log_file, event)
         print(decode)
@@ -142,22 +143,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
                 aEjecutar = "./mp32rtp -i " + My_IP + " -p "
                 aEjecutar += RTP_PORT
                 aEjecutar += " < " + audio_file
-                print("Ejecutando archivo:", aEjecutar)
+                print("Running file:", aEjecutar)
                 os.system(aEjecutar)
                 ACK = "ACK sip:" + OPTION + " SIP/2.0\r\n"
-                print("Enviando: " + ACK)
-                my_socket.send(bytes(ACK, "utf-8"))
-                event = ' Received from ' + Proxy_IP + ':'
-                event += str(Proxy_Port) + ': ' + data.decode('utf-8')
+                print("Sending: " + ACK)
+                my_socket.send(bytes(ACK, 'utf-8'))
+                event = " Sent to " + Proxy_IP + ":"
+                event += str(Proxy_Port) + ': ' + ACK
                 log(log_file, event)
             elif DECODED[1] == "404":
                 sys.exit("404 User Not Found")
 
     elif METHOD == "BYE":
         LINE = METHOD + " sip:" + OPTION + " SIP/2.0\r\n"
-        print("Enviando: " + LINE)
-        my_socket.send(bytes(LINE, "utf-8") + b"\r\n")
-        event = ' Sent to ' + Proxy_IP + ':'
+        print("Sending: " + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b"\r\n")
+        event = " Sent to " + Proxy_IP + ":"
         event += str(Proxy_Port) + ': ' + LINE
         log(log_file, event)
 
